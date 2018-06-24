efbb16ecf8601eddf69ef62140ab147614bd514a
josdem <joseluis.delacruz@gmail.com>
2018-06-24 15:11:33 -0400

    Adding WebClient bean

--- src/main/java/com/jos/dem/webclient/WebClientApplication.java
+++ src/main/java/com/jos/dem/webclient/WebClientApplication.java
@@ -1,12 +1,20 @@
 package com.jos.dem.webclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class WebClientApplication {
 
+  @Bean
+  public WebClient webClient() {
+    return WebClient.create("https://api.github.com/users/josdem/keys");
+  }
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebClientApplication.class, args);
 	}
+
 }

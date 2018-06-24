f064ecccee304bf7dbe967f44ae97146c3894d9b
josdem <joseluis.delacruz@gmail.com>
2018-06-24 14:12:26 -0400

    Moving to the right package structure

--- src/main/java/com/jos/dem/webclient/WebClientApplication.java
+++ src/main/java/com/jos/dem/webclient/WebClientApplication.java
@@ -1,12 +1,12 @@
-package com.example.webclientworkshop;
+package com.jos.dem.webclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WebClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WebClientApplication.class, args);
 	}
 }

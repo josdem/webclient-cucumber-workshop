be4be82232b17dea8f40cfff29bcba2d54bac4d9
josdem <joseluis.delacruz@gmail.com>
2018-06-28 20:10:58 -0400

    #1 Adding cucumber test case

---
+++ src/test/java/com/jos/dem/webclient/CucumberTest.java
@@ -0,0 +1,9 @@
+package com.jos.dem.webclient;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class CucumberTest {}

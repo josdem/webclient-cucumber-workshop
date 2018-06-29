d0db70d119d86a8370bf6bd00c89bbb5a5bf1f2c
josdem <joseluis.delacruz@gmail.com>
2018-06-29 09:56:05 -0400

    #2 Adding public email model

---
+++ src/main/java/com/jos/dem/webclient/model/PublicEmail.java
@@ -0,0 +1,15 @@
+package com.jos.dem.webclient.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PublicEmail {
+  private String email;
+  private boolean verified;
+  private boolean primary;
+  private String visibility;
+}

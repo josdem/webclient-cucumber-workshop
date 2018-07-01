9035a532e2f2010f36086167fa82c7d365db1d90
josdem <joseluis.delacruz@gmail.com>
2018-07-01 15:41:15 -0400

    #3 Adding label post test definition

--- src/main/java/com/jos/dem/webclient/service/impl/LabelServiceImpl.java
+++ src/main/java/com/jos/dem/webclient/service/impl/LabelServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import com.jos.dem.webclient.model.Label;
 import com.jos.dem.webclient.model.LabelResponse;
 import com.jos.dem.webclient.service.LabelService;
 
@@ -14,9 +15,17 @@ public class LabelServiceImpl implements LabelService {
   @Autowired
   private WebClient webClient;
 
+  private Label label;
+
+  @PostConstruct
+  public void setup(){
+    label = new Label("cucumber", "Cucumber is a very powerful testing framework written in the Ruby programming language","#ed14c5");
+  }
+
   public Flux<LabelResponse> create() {
-    return webClient.get().uri("/repos/josdem/webclient-workshop/labels").retrieve()
-    .bodyToFlux(LabelResponse.class);
+    return webClient.post()
+      .uri("/repos/josdem/webclient-workshop/labels").retrieve()
+      .body(LabelResponse.class);
   }
 
 }

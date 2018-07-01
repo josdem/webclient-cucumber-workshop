0acc5111e00a5d725fee029aad33883b423d5894
josdem <joseluis.delacruz@gmail.com>
2018-07-01 13:46:02 -0400

    \\#3 Adding label model

---
+++ src/main/java/com/jos/dem/webclient/model/Label.java
@@ -0,0 +1,14 @@
+package com.jos.dem.webclient.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Label {
+  private String name;
+  private description;
+  private color;
+}

7034d1f3bdb39c16f021002727fd1bcf29f3fb9a
josdem <joseluis.delacruz@gmail.com>
2018-07-08 15:15:58 -0400

    #4 Adding delete service method

--- src/main/java/com/jos/dem/webclient/service/LabelService.java
+++ src/main/java/com/jos/dem/webclient/service/LabelService.java
@@ -2,9 +2,11 @@ package com.jos.dem.webclient.service;
 
 import reactor.core.publisher.Mono;
 import com.jos.dem.webclient.model.LabelResponse;
+import org.springframework.web.reactive.function.client.ClientResponse;
 
 public interface LabelService {
 
   Mono<LabelResponse> create();
+  Mono<ClientResponse> delete();
 
 }

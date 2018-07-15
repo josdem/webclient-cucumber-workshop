80fa0bd030f43179c123fd3acc39d4e3eadcc043
josdem <joseluis.delacruz@gmail.com>
2018-07-15 12:25:19 -0400

    Deleting unused imports

--- src/test/java/com/jos/dem/webclient/LabelPostTest.java
+++ src/test/java/com/jos/dem/webclient/LabelPostTest.java
@@ -2,14 +2,9 @@ package com.jos.dem.webclient;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.jos.dem.webclient.model.LabelResponse;
 
-import java.util.List;
-
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import reactor.core.publisher.Flux;
 
 public class LabelPostTest extends LabelIntegrationTest {
 

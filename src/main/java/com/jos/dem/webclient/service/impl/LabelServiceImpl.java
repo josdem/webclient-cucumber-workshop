2e2a12129667e944425299161f9b2ff79ad5d8a8
josdem <joseluis.delacruz@gmail.com>
2018-08-02 10:20:14 -0400

    Moving to body insterters, in that way we can avoid dto imports

--- src/main/java/com/jos/dem/webclient/service/impl/LabelServiceImpl.java
+++ src/main/java/com/jos/dem/webclient/service/impl/LabelServiceImpl.java
@@ -2,16 +2,16 @@ package com.jos.dem.webclient.service.impl;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 
-import com.jos.dem.webclient.model.Label;
 import com.jos.dem.webclient.model.LabelResponse;
 import com.jos.dem.webclient.service.LabelService;
 import com.jos.dem.webclient.util.LabelCreator;
@@ -30,7 +30,7 @@ public class LabelServiceImpl implements LabelService {
   public Mono<LabelResponse> create() {
     return webClient.post()
       .uri(githubLabelsPath).accept(APPLICATION_JSON)
-      .body(Mono.just(labelCreator.create()), Label.class)
+      .body(BodyInserters.fromObject(labelCreator.create()))
       .retrieve()
       .bodyToMono(LabelResponse.class);
   }
@@ -38,7 +38,7 @@ public class LabelServiceImpl implements LabelService {
   public Mono<ClientResponse> update(String name){
     return webClient.patch()
       .uri(githubLabelsPath + "/" + name).accept(APPLICATION_JSON)
-      .body(Mono.just(labelCreator.update()), Label.class)
+      .body(BodyInserters.fromObject(labelCreator.update()))
       .exchange();
   }
 

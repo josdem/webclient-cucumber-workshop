d7e20d0bf15ecd36d9cce04a108a817088cc7afa
josdem <joseluis.delacruz@gmail.com>
2018-09-16 09:24:36 -0400

    Deleting unused annotation

--- src/main/java/com/jos/dem/webclient/model/Label.java
+++ src/main/java/com/jos/dem/webclient/model/Label.java
@@ -1,12 +1,10 @@
 package com.jos.dem.webclient.model;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Label {
   private String name;
   private String description;

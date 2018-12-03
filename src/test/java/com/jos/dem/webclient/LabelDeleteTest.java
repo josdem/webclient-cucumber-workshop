dc56e1c5847f00130cc5e2154116036e7cdef873
josdem <joseluis.delacruz@gmail.com>
2018-12-03 16:47:21 -0500

    #6 Adding before and after methods in delete label

--- src/test/java/com/jos/dem/webclient/LabelDeleteTest.java
+++ src/test/java/com/jos/dem/webclient/LabelDeleteTest.java
@@ -3,19 +3,38 @@ package com.jos.dem.webclient;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.springframework.web.reactive.function.client.ClientResponse;
 import java.util.List;
 
+import org.springframework.web.reactive.function.client.ClientResponse;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LabelDeleteTest extends LabelIntegrationTest {
 
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Before
+  public void setup() {
+    log.info("Before any test execution");
+  }
+
   @Then("^User deletes label$")
   public void shouldDeleteLabel() throws Exception {
+    log.info("Running: User deletes label");
     ClientResponse response = delete("spock")
       .block();
 
     assertEquals(NO_CONTENT, response.statusCode(), "Should delete label");
+  }
+
+  @After
+  public void tearDown() {
+    log.info("After all test execution");
   }
 
 }

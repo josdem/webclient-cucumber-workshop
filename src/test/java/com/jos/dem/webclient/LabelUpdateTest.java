b331770c63ddf4d868ca35a8e6834b3b13c23f9b
josdem <joseluis.delacruz@gmail.com>
2018-12-03 16:57:16 -0500

    #6 Adding before and after methods in update label

--- src/test/java/com/jos/dem/webclient/LabelUpdateTest.java
+++ src/test/java/com/jos/dem/webclient/LabelUpdateTest.java
@@ -6,16 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import java.util.List;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LabelUpdateTest extends LabelIntegrationTest {
 
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Before
+  public void setup() {
+    log.info("Before any test execution");
+  }
+
   @Then("^User updates label$")
   public void shouldCreateLabel() throws Exception {
+    log.info("Running: User updates label");
+
     ClientResponse response = update("cucumber")
       .block();
 
     assertEquals(OK, response.statusCode(), "Should update to spock information");
+  }
+
+  @After
+  public void tearDown() {
+    log.info("After all test execution");
   }
 
 }
